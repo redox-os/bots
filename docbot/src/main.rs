6cef045a807d1bf35d8563a2dff6642699dff288
ticki <ticki@users.noreply.github.com>
2016-04-12 21:11:20 +0200

    Add docbot

---
+++ docbot/src/main.rs
@@ -0,0 +1,15 @@
+extern crate mars;
+
+use mars::{Bot, Response};
+
+fn main() {
+    Bot::new(env!("TOKEN"), move |req| {
+        let query = req.text.trim_left_matches("docbot:").trim_left_matches("docgen ").trim();
+
+        Response {
+            username: Some("docbot"),
+            text: format!("[\"{}\"](https://doc.rust-lang.org/nightly/std/?search={})", query, query),
+            icon_url: Some("http://www.modulesca.com/phototeque/Documentation_6.jpg"),
+        }
+    }).init(env!("IP")).unwrap();
+}
