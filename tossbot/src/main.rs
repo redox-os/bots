3ce6bb3b332b756aed4af9723b79a165c3e3ecfb
ticki <ticki@users.noreply.github.com>
2016-04-12 19:54:55 +0200

    Cointoss fixes

--- tossbot/src/main.rs
+++ tossbot/src/main.rs
@@ -6,16 +6,14 @@ use std::io::Read;
 use std::sync::{Arc, Mutex};
 
 fn main() {
-    let rand = Arc::new(Mutex::new(fs::File::open("/dev/random").unwrap()
-        .bytes()
-        .filter_map(|x| {
-            x.ok().map(|x| if x & 1 == 0 { Some(()) } else { None })
-        })));
+    let rand = Arc::new(Mutex::new(fs::File::open("/dev/random").unwrap()));
 
     Bot::new(env!("TOKEN"), move |_| {
+        let mut outcome = [0];
+        rand.lock().unwrap().read_exact(&mut outcome).unwrap();
         Response {
             username: Some("tossbot"),
-            text: if rand.lock().unwrap().next().is_some() { ":+1:".into() } else { ":-1:".into() },
+            text: if outcome[0] % 2 == 0 { ":+1:".into() } else { ":-1:".into() },
             icon_url: Some("https://upload.wikimedia.org/wikipedia/commons/2/28/Coin_of_Mysia_4th_century_BCE.jpg"),
         }
     }).init(env!("IP")).unwrap();
